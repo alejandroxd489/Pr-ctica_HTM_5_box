9f019805a71eec3e4dcd23d1d0d86203f5d41807
alejandroxd489 <alejandroxd489@gmail.com>
2026-04-16 10:19:27 -0600

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log(" Página de Boxeo cargada correctamente");
+
+/* Scroll suave tipo app */
+document.querySelectorAll('.nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
